2a68e1a7b0288fd7acd89da300ecaba01b209785
myskotima130 <myskotima130@gmail.com>
2024-09-30 14:45:11 -0400

    create schema

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { typeDefs } from "./schema";
+
+const server = new ApolloServer({ typeDefs });
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
+});
+
+console.log(`🚀 Server ready at ${url}`);
