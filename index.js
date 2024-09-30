700262b25a6518fcad6f99d8db753e899e31a20e
myskotima130 <myskotima130@gmail.com>
2024-09-30 16:08:02 -0400

    make it js to run the server

---
+++ index.js
@@ -0,0 +1,23 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./_db.js";
+
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    reviews: () => db.reviews,
+    authors: () => db.authors,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
+});
+
+console.log(`🚀 Server ready at ${url}`);
