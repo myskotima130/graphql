ca3630bcfebf9d782b71a44f3040e2fb3e1daf1a
myskotima130 <myskotima130@gmail.com>
2024-10-01 13:31:05 -0400

    add mutations

--- index.js
+++ index.js
@@ -7,8 +7,36 @@ import { typeDefs } from "./schema.js";
 const resolvers = {
   Query: {
     games: () => db.games,
+    game: (_, args) => db.games.find((game) => game.id === args.id),
     reviews: () => db.reviews,
+    review: (_, args) => db.reviews.find((review) => review.id === args.id),
     authors: () => db.authors,
+    author: (_, args) => db.authors.find((author) => author.id === args.id),
+  },
+  Game: {
+    reviews: (parent) =>
+      db.reviews.filter((review) => review.game_id === parent.id),
+  },
+  Author: {
+    reviews: (parent) =>
+      db.reviews.filter((review) => review.author_id === parent.id),
+  },
+  Review: {
+    author: (parent) =>
+      db.authors.find((author) => author.id === parent.author_id),
+    game: (parent) => db.games.find((game) => game.id === parent.game_id),
+  },
+  Mutation: {
+    deleteGame: (_, args) => db.games.filter((game) => game.id !== args.id),
+    addGame: (_, args) => {
+      const newGame = { ...args.game, id: crypto.randomUUID() };
+      db.games.push(newGame);
+      return newGame;
+    },
+    updateGame: (_, args) =>
+      db.games.map((game) =>
+        game.id === args.id ? { ...game, ...args.edits } : game
+      ),
   },
 };
 
--- schema.js
+++ schema.js
@@ -3,20 +3,40 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
     type Query {
        reviews: [Review]
+       review(id: ID!): Review
        games: [Game]
+       game(id: ID!): Game
        authors: [Author]
+       author(id: ID!): Author
+    }
+    type Mutation {
+        deleteGame(id: ID!): [Game]
+        addGame(game: addGameInput!): Game
+        updateGame(id: ID!, edits: editGameInput!): [Game]
+    }
+    input addGameInput {
+        title: String!
+        platform: [String!]!
+    }
+    input editGameInput {
+        title: String
+        platform: [String!]
     }
 `;
